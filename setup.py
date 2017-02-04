ec30d1cd577587181af29dc1f9ac7376ac42f8fe
shanahanjrs <shanahan.jrs@gmail.com>
2017-02-03 22:14:34 -0500

    Renamed vikicli to viki to make life easier

--- setup.py
+++ setup.py
@@ -51,11 +51,11 @@ setup(
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
-    entry_points={
-        'console_scripts': [
-            'viki=viki:main',
-        ],
-    }
-    #scripts = ['viki']
+    #entry_points={
+        #'console_scripts': [
+            #'viki=viki:main',
+        #],
+    #}
+    scripts = ['bin/vikid', 'bin/viki']
 
 )
