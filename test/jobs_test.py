1657995e2e8131b13ed3d1171c7a75461c7c7a10
shanahanjrs <shanahan.jrs@gmail.com>
2017-02-08 23:48:28 -0500

    Added main test class to jobs_test.py

--- test/jobs_test.py
+++ test/jobs_test.py
@@ -10,85 +10,87 @@ job = Jobs()
 
 ### Test private functions
 
-def test_quote_string_single_quotes():
+class TestClass:
 
-    test_variables = [
-        'x', '"x', 'x""x', ' -- x', '\'\'\n'
-    ]
+    def test_quote_string_single_quotes():
 
-    for var in test_variables:
-        assert job._quote_string(var) == "'" + var + "'"
+        test_variables = [
+            'x', '"x', 'x""x', ' -- x', '\'\'\n'
+        ]
 
-
-def test_quote_string_double_quotes():
-
-    test_variables = [
-        'x', '"x', 'x""x', ' -- x', '\'\'\n'
-    ]
-
-    for var in test_variables:
-        assert job._quote_string(var, SingleQuote=False) == '"' + var + '"'
+        for var in test_variables:
+            assert job._quote_string(var) == "'" + var + "'"
 
 
-def test_write_job_file():
+    def test_quote_string_double_quotes():
 
-    job_file_path = '/tmp/pytest-job-file.txt'
-    job_file_text = 'This is a test message'
+        test_variables = [
+            'x', '"x', 'x""x', ' -- x', '\'\'\n'
+        ]
 
-    assert job._write_job_file(job_file_path, job_file_text) is not None
-
-
-def test_read_job_file():
-
-    job_file_path = '/tmp/pytest-job-file.txt'
-    job_file_text = 'This is a test message'
-
-    assert job._read_job_file(job_file_path) is not None
-
-    assert job._dirty_rm_rf(job_file_path) is not None
+        for var in test_variables:
+            assert job._quote_string(var, SingleQuote=False) == '"' + var + '"'
 
 
-def test_run_shell_command():
+    def test_write_job_file():
 
-    command = "echo Shanahanjrs @0xJRS"
-    tmp_filename = '/tmp/pytest-job-run-file.txt'
+        job_file_path = '/tmp/pytest-job-file.txt'
+        job_file_text = 'This is a test message'
 
-    assert job._run_shell_command(command, tmp_filename) == (True, 0)
+        assert job._write_job_file(job_file_path, job_file_text) is not None
 
-    assert job._dirty_rm_rf(tmp_filename) is not None
+
+    def test_read_job_file():
+
+        job_file_path = '/tmp/pytest-job-file.txt'
+        job_file_text = 'This is a test message'
+
+        assert job._read_job_file(job_file_path) is not None
+
+        assert job._dirty_rm_rf(job_file_path) is not None
+
+
+    def test_run_shell_command():
+
+        command = "echo Shanahanjrs @0xJRS"
+        tmp_filename = '/tmp/pytest-job-run-file.txt'
+
+        assert job._run_shell_command(command, tmp_filename) == (True, 0)
+
+        assert job._dirty_rm_rf(tmp_filename) is not None
 
 ### Test public functions
 
-def test_get_jobs():
+    def test_get_jobs():
 
-    assert job.get_jobs()["success"] == 1
-
-
-def test_create_job():
-
-    job_name = 'viki-pytest-job-00'
-    job_steps = ["echo Shanahanjrs", "pwd", "uname -a"]
-    job_config = {"description":"This is my test job", "steps":job_steps}
-
-    assert job.create_job(job_name, job_config)["success"] == 1
+        assert job.get_jobs()["success"] == 1
 
 
-def test_get_job_by_name():
+    def test_create_job():
 
-    job_name = 'viki-pytest-job-00'
+        job_name = 'viki-pytest-job-00'
+        job_steps = ["echo Shanahanjrs", "pwd", "uname -a"]
+        job_config = {"description":"This is my test job", "steps":job_steps}
 
-    assert job.get_job_by_name(job_name)["success"] == 1
-
-
-def test_update_job_by_name():
-
-    job_name = 'viki-pytest-job-00'
-
-    assert job.update_job(job_name)["message"] == "-- Under Construction --"
+        assert job.create_job(job_name, job_config)["success"] == 1
 
 
-def test_delete_job_by_name():
+    def test_get_job_by_name():
 
-    job_name = 'viki-pytest-job-00'
+        job_name = 'viki-pytest-job-00'
 
-    assert job.delete_job(job_name)["success"] == 1
+        assert job.get_job_by_name(job_name)["success"] == 1
+
+
+    def test_update_job_by_name():
+
+        job_name = 'viki-pytest-job-00'
+
+        assert job.update_job(job_name)["message"] == "-- Under Construction --"
+
+
+    def test_delete_job_by_name():
+
+        job_name = 'viki-pytest-job-00'
+
+        assert job.delete_job(job_name)["success"] == 1
