15c423f82b1603e770209ec5a71135c2a228a0b4
John Shanahan <jshanahan@eagleeyeintelligence.com>
2017-02-11 16:14:52 -0500

    Fixes for when you call viki setup from the command line

--- src/_conf.py
+++ src/_conf.py
@@ -5,5 +5,7 @@ Provides Viki configuration information.
 __config_home__ = "/usr/local/viki"
 __config_jobs_dir__ = __config_home__ + "/" + "jobs"
 __config_filename__ = "config.json"
+__config_file_path__ = __config_home__ + "/" + __config_filename__
+__logfile_path__ = __config_home__ + "/" + "logs"
 
-__all__ = ['config_home', 'config_file']
+__all__ = ["__config_home__", "__config_jobs_dir__", "__config_filename__", "__config_file_path__", "__logfile_path__" ]
