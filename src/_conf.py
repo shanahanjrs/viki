41ccc3df71956abe8c15fba070cd0db21709e82d
shanahanjrs <shanahan.jrs@gmail.com>
2017-02-14 22:21:34 -0500

    Preparing to allow parameterized build runs and cli implementation of job creation

--- src/_conf.py
+++ src/_conf.py
@@ -4,7 +4,7 @@ Provides Viki configuration information.
 
 __config_home__ = "/usr/local/viki"
 __config_jobs_dir__ = __config_home__ + "/" + "jobs"
-__config_filename__ = "config.json"
+__config_filename__ = "viki.json"
 __config_file_path__ = __config_home__ + "/" + __config_filename__
 __logfile_path__ = __config_home__ + "/" + "logs"
 
